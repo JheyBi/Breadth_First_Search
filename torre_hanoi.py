d9c9c87bb2f3aeab577d64f228a1c8d842715021
JheyBi <joaodlrio@outlook.com>
2023-07-26 23:13:58 -0300

    Atualização Torre Hanoi e Criação Puzzle 8

--- torre_hanoi.py
+++ torre_hanoi.py
@@ -4,28 +4,59 @@
 # 3º Expadir os valores de k, onde SE k==i e k==j, não expande.Além disso os a expansão também não pode ser igual a i nem igual a j
 
 import networkx as nx
+import itertools
 
 grafo = nx.Graph()
 
-
 def grafo_hanoi(n_discos):
-    for i in range(1, n_discos+1):
-        for j in range(1, n_discos+1):
-            for k in range(1, n_discos+1):
-                aux=1
-                while aux < n_discos+1:
+    # Gerar todas as permutações com repetição dos números de 1 a n_discos
+    permutations = list(itertools.product(list(range(1, 4)), repeat=n_discos))
+    
+    # Percorrer as permutações e atribuir os valores às variáveis
+    for perm in permutations:
+        i, j, k, l, m = perm
+        aux=1
+        while aux < 4:
+            if aux!=i:
+                grafo.add_edge((i, j, k, l, m), (aux, j, k, l, m))
+            if aux!=j:
+                if j!=i:
                     if aux!=i:
-                        grafo.add_edge((i, j, k), (aux, j, k))
-                    if aux!=j:
-                        if j!=i:
-                            if aux!=i:
-                                grafo.add_edge((i, j, k), (i, aux, k))
-                    if aux!=k:
-                        if k!=i and k!=j:
-                            if aux!=i and aux!=j:
-                                grafo.add_edge((i, j, k), (i, j, aux))
-                    aux+=1
+                        grafo.add_edge((i, j, k, l, m), (i, aux, k, l, m))
+            if aux!=k:
+                if k!=i and k!=j:
+                    if aux!=i and aux!=j:
+                        grafo.add_edge((i, j, k, l, m), (i, j, aux, l, m))
+            if aux!=l:
+                if l!=i and l!=j and l!=k:
+                    if aux!=i and aux!=j and aux!=k:
+                        grafo.add_edge((i, j, k, l, m), (i, j, k, aux, m))
+            if aux!=m:
+                if m!=i and m!=j and m!=k and m!=l:
+                    if aux!=i and aux!=j and aux!=k and aux!=l:
+                        grafo.add_edge((i, j, k, l, m), (i, j, k, l, aux))
+            aux+=1
     return grafo
+
+
+# def grafo_hanoi(n_discos):
+#     for i in range(1, n_discos+1):
+#         for j in range(1, n_discos+1):
+#             for k in range(1, n_discos+1):
+#                 aux=1
+#                 while aux < n_discos+1:
+#                     if aux!=i:
+#                         grafo.add_edge((i, j, k), (aux, j, k))
+#                     if aux!=j:
+#                         if j!=i:
+#                             if aux!=i:
+#                                 grafo.add_edge((i, j, k), (i, aux, k))
+#                     if aux!=k:
+#                         if k!=i and k!=j:
+#                             if aux!=i and aux!=j:
+#                                 grafo.add_edge((i, j, k), (i, j, aux))
+#                     aux+=1
+#     return grafo
 
 def busca_em_extensão(grafo, inicio, objetivo):
     #Define a fila de busca
@@ -112,17 +143,17 @@ def grafo_to_dict(grafo):
     return nx.to_dict_of_lists(grafo)
 
 def main():
-    grafo_hanoi(3)
+    grafo_hanoi(5)
     dic = grafo_to_dict(grafo)
-    # Filhos de (1,1,2)
-    print("Filhos de (1,1,2): ", dic[(1,1,2)])
-    
-    # Busca em extensão
-    print("Busca em Extensão: ", busca_em_extensão(dic, (1,1,1), (3,3,3)))
+    print(len(dic))
+    origem = (1,1,1,1,1)
+    objetivo = (3,3,3,3,3)
+
+    # # Busca em extensão
+    print("Busca em Extensão: ", busca_em_extensão(dic, origem, objetivo))
 
     # Busca em profundidade
-    origem = (1,1,1)
-    objetivo = (3,3,3)
+    
     solucao = dfs(dic, origem, objetivo)
     if solucao != False:
         caminho = [objetivo]
@@ -143,16 +174,16 @@ def main():
     else:
         print("\n\nBusca em Profundidade Limitada: Não foi encontrado solução")
 
-    # Busca em profundidade iterativa
-    solucao = ids(dic, origem, objetivo, 22)
-    if solucao != False:
-        caminho = [objetivo]
-        while origem != objetivo:
-            caminho.insert(0, solucao[objetivo])
-            objetivo = solucao[objetivo]
-        print("\n\nBusca em Profundidade Iterativa: ", caminho)
-    else:
-        print("\n\nBusca em Profundidade Iterativa: Não foi encontrado solução")
+    # # Busca em profundidade iterativa
+    # solucao = ids(dic, origem, objetivo, 22)
+    # if solucao != False:
+    #     caminho = [objetivo]
+    #     while origem != objetivo:
+    #         caminho.insert(0, solucao[objetivo])
+    #         objetivo = solucao[objetivo]
+    #     print("\n\nBusca em Profundidade Iterativa: ", caminho)
+    # else:
+    #     print("\n\nBusca em Profundidade Iterativa: Não foi encontrado solução")
     
 
             
